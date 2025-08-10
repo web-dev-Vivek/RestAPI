bd4c2b4ca42dec9bdf65a52a2f1f0c7f36f880ec
Vivek Jha <progamervivek2020@gmail.com>
2025-08-10 14:32:43 +0530

    .

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/users", (req, res) => {
   return res.json(users);
@@ -37,7 +40,11 @@ app
   .delete((req, res) => {});
 
 app.post("/api/users", (req, res) => {
-  res.json({ status: "pending" });
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    res.json({ status: "success", id: users.length });
+  });
 });
 
 app.listen(port, () => {
